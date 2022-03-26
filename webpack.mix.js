3dbb558e1945f1e80deb47b6877b88e0c963132e
mahdi.aslami <mahdi.aslami@partsoftware.com>
2022-03-26 16:20:57 +0430

    Initilize vue.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.webpackConfig(
+    require('./webpack.config')
+)
+
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ])
+
+mix.js('resources/js/vue.js', 'public/js')
+    .vue({ version: 3 })
+    .alias({ '@': 'resources/js' })
